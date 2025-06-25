e1b8408a035e612304ca522267efc3be38685bcb
Shubha5679 <shubhaadev@gmail.com>
2025-06-25 15:35:32 +0530

    Program - 4.java

---
+++ Program - 4.java
@@ -0,0 +1,28 @@
+import java.util.*;
+public class CountOfMultiples
+{
+	public static void main(String[] args)
+	{
+		int[]  list = { 1, 2, 8, 9, 12, 46, 76, 82, 15, 20, 30};
+		
+		Map<Integer, Integer>  count = new HashMap<>();
+		
+		for(int i=1; i<=9; i++)
+		{
+			count.put(i, 0);
+		}
+
+		for(int key=1; key<=9; key++)
+		{
+		    for(int num : list)
+		    {
+    			if(num % key ==0)
+    			{
+    				count.put(key, count.get(key) + 1);
+    			}
+		    }
+			
+		}
+		System.out.println(count);
+	}
+}
