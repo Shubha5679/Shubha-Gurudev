4b6af6f026ac72150221450c51034e5dfc5c0e40
Shubha5679 <shubhaadev@gmail.com>
2025-06-25 15:26:04 +0530

    Program - 3.java

---
+++ Program - 3.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public class OddSeriesGenerator 
+{
+	public static void main(String[] args)
+	{
+		Scanner scanner = new Scanner(System.in);
+		int result =0;
+
+		System.out.println(" Enter  integer as input ");
+		int  num = scanner.nextInt();
+		
+		int oddOrEven = (num % 2 != 0) ? num : num -1;
+
+		for(int i=1; i<(oddOrEven*2); i++)
+		{
+			if(i % 2 != 0)
+			{
+				System.out.print( i + ", " );
+			}
+		}
+		scanner.close();
+	}
+}
+			
